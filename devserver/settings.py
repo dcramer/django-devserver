06eccfc75e7aa028ca29d9e7f85ae36f23e6bea9
David Cramer <dcramer@David-Cramers-MacBook.local>
2010-02-10 12:12:41 -0600

    Now settings actually work

--- devserver/settings.py
+++ devserver/settings.py
@@ -1,10 +1,12 @@
-DEVSERVER_MODULES = (
+from django.conf import settings
+
+DEVSERVER_MODULES = getattr(settings, 'DEVSERVER_MODULES', (
     'devserver.modules.sql.SQLRealTimeModule',
     'devserver.modules.profile.ProfileSummaryModule',
     'devserver.modules.sql.SQLSummaryModule',
     # 'devserver.modules.profile.LeftOversModule',
     # 'devserver.modules.cache.CacheSummaryModule',
-)
+))
 
 # This variable gets set to True when we're running the devserver
 DEVSERVER_ACTIVE = False
