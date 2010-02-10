5ca0e0683a663271c40d728e5f88ee19a26eca61
David Cramer <dcramer@David-Cramers-MacBook.local>
2010-02-10 00:52:19 -0600

    Add ProfileSummary to defaults

--- devserver/settings.py
+++ devserver/settings.py
@@ -1,6 +1,7 @@
 DEVSERVER_MODULES = (
     'devserver.modules.sql.SQLRealTimeModule',
-    'devserver.modules.cache.CacheSummaryModule',
+    'devserver.modules.profile.ProfileSummaryModule',
+    # 'devserver.modules.cache.CacheSummaryModule',
 )
 
 # This variable gets set to True when we're running the devserver
