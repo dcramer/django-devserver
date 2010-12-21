61ed91f919adfbcb7863aeaad8266a061ed59d95
David Cramer <dcramer@gmail.com>
2010-12-20 17:40:13 -0800

    Ensure we're setting up the database and handle the request properly

--- devserver/testcases.py
+++ devserver/testcases.py
@@ -1,4 +1,6 @@
+from django.conf import settings
 from django.core.handlers.wsgi import WSGIHandler
+from django.core.management import call_command
 from django.core.servers.basehttp import StoppableWSGIServer, AdminMediaHandler, WSGIServerException
 from django.test.testcases import TestServerThread
 
@@ -23,3 +25,16 @@ class ThreadedTestServerThread(TestServerThread):
             self.error = e
             self.started.set()
             return
+        
+        # Must do database stuff in this new thread if database in memory.
+        if settings.DATABASE_ENGINE == 'sqlite3' \
+            and (not settings.TEST_DATABASE_NAME or settings.TEST_DATABASE_NAME == ':memory:'):
+            # Import the fixture data into the test database.
+            if hasattr(self, 'fixtures'):
+                # We have to use this slightly awkward syntax due to the fact
+                # that we're using *args and **kwargs together.
+                call_command('loaddata', *self.fixtures, **{'verbosity': 0})
+
+        # Loop until we get a stop event.
+        while not self._stopevent.isSet():
+            httpd.handle_request()
