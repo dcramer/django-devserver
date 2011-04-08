f206340841bd5be2d21a523b6bcaf45921c0669f
German M. Bravo <german.mb@deipi.com>
2011-04-08 14:36:51 -0500

    Added staticfiles support and updated runserver

--- devserver/middleware.py
+++ devserver/middleware.py
@@ -4,6 +4,9 @@ class DevServerMiddleware(object):
     def should_process(self, request):
         from django.conf import settings
         
+        if getattr(settings, 'STATIC_URL') and request.path.startswith(settings.STATIC_URL):
+            return False
+
         if settings.MEDIA_URL and request.path.startswith(settings.MEDIA_URL):
             return False
         
