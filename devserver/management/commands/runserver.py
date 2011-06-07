6cf3e12ecd16c9d26a50b5d6d4ef635dbd61f2c4
Nikolai Prokoschenko <nikolai.prokoschenko@arbeitsagentur.de>
2011-06-07 13:11:45 +0200

    Rename setting variables to match other existing devserver settings.

--- devserver/management/commands/runserver.py
+++ devserver/management/commands/runserver.py
@@ -51,8 +51,8 @@ class Command(BaseCommand):
         from django.conf import settings
 
         if not addrport:
-            addr = settings.get("RUNSERVER_DEFAULT_ADDR", '')
-            port = settings.get("RUNSERVER_DEFAULT_PORT", '8000')
+            addr = settings.get("DEVSERVER_DEFAULT_ADDR", '')
+            port = settings.get("DEVSERVER_DEFAULT_PORT", '8000')
         else:
             try:
                 addr, port = addrport.split(':')
